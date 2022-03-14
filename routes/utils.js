01eabe0585f0365c67df9846687e5b379125d952
Rami Martinez <40069890+rammartinez00@users.noreply.github.com>
2022-03-14 16:46:12 -0600

    sign up & login routes

---
+++ routes/utils.js
@@ -0,0 +1,81 @@
+const csrf = require("csurf");
+const csrfProtection = csrf({ cookie: true });
+const { check, validationResult } = require("express-validator");
+const db = require("../db/models");
+
+const asyncHandler = (handler) => (req, res, next) =>
+  handler(req, res, next).catch(next);
+
+const userValidator = [
+  check("fullName")
+    .exists({ checkFalsy: true })
+    .withMessage("Please provide a value for full name"),
+  check("userName")
+    .exists({ checkFalsy: true })
+    .withMessage("Please provide a value for full name")
+    .isLength({ max: 50 })
+    .withMessage("Username must not be longer than 50 characters")
+    .custom((value) => {
+      return db.User.findOne({ where: { userName: value } }).then((user) => {
+        if (user) {
+          return Promise.reject(
+            "The provided Username is already in use by another account"
+          );
+        }
+      });
+    }),
+  check("email")
+    .exists({ checkFalsy: true })
+    .withMessage("Please provide a value for email address")
+    .isEmail()
+    .withMessage("Email Address is not a valid email")
+    .custom((value) => {
+      return db.User.findOne({ where: { email: value } }).then((user) => {
+        if (user) {
+          return Promise.reject(
+            "The provided Email Address is already in use by another account"
+          );
+        }
+      });
+    }),
+  check("password")
+    .exists({ checkFalsy: true })
+    .withMessage("Please provide a value for password")
+    .isLength({ max: 50 })
+    .withMessage("password must not be more than 50 characters long")
+    .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])/, "g")
+    .withMessage(
+      'Password must contain at least 1 lowercase letter, uppercase letter, number, and special character (i.e. "!@#$%^&*")'
+    ),
+  check("confirmPassword")
+    .exists({ checkFalsy: true })
+    .withMessage("Please provide a value for confirm password")
+    .isLength({ max: 50 })
+    .withMessage("confirm password must not be more than 50 characters long")
+    .custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error("Confirm Password does not match Password");
+      }
+      return true;
+    })
+    .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])/, "g")
+    .withMessage(
+      'Password must contain at least 1 lowercase letter, uppercase letter, number, and special character (i.e. "!@#$%^&*")'
+    ),
+];
+
+const loginValidators = [
+  check("email")
+    .exists({ checkFalsy: true })
+    .withMessage("Please provide a value for Email Address"),
+  check("password")
+    .exists({ checkFalsy: true })
+    .withMessage("Please provide a value for Password"),
+];
+
+module.exports = {
+  csrfProtection,
+  asyncHandler,
+  userValidator,
+  loginValidators,
+};
