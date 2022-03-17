08e3b71077621434b8c683cff4980bd0fec9d917
Huyen Nguyen <hn078722@gmail.com>
2022-03-16 21:29:28 -0700

    adding js for button and modifying the pets box

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,6 +1,16 @@
 
-window.addEventListener("DOMcontentload", (event)=>{
-    //likesButton = 
+window.addEventListener("DOMContentLoaded", (event)=>{
+    let likesValue = 0;
+
+    const likes = document.getElementById("likes-value")
+    likes.innerHTML = likesValue
+
+    document
+        .getElementById("like-button")
+        .addEventListener("click", event => {
+            likesValue += 1
+            likes.innerHTML = likesValue;
+        })
 
 
 })
