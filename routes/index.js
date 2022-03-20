7b47a4404ee95c780c5f17e88065926cb229da23
Rami Martinez <40069890+rammartinez00@users.noreply.github.com>
2022-03-19 23:20:41 -0600

    Revert " the millionth fix of the like button"

--- routes/index.js
+++ routes/index.js
@@ -12,49 +12,22 @@ router.get('/', asyncHandler(async (req, res, next) => {
     include: [db.PetLike]
   })
 
-  // if (user) {
-  //   petArr = pets.map(pet => {
-  //     if (pet.PetLikes.length) {
-  //       for (let i = 0; i < pet.PetLikes.length; i++) {
-  //         if (pet.PetLikes[i].userId === user.id) {
-  //           return { exists: pet.PetLikes[i].id }
-  //         } else {
-  //           console.log('=========HERRRRRRRRRRRRRREE')
-  //           return { exists: false }
-  //         }
-
-  //       }
-  //     } else {
-  //       console.log('========ELSEEEEEEEEEEE')
-  //       return { exists: false }
-  //     }
-  //   })
-  // } else {
-  //   petArr = [{ exists: false }]
-  // }
-
-
   if (user) {
     petArr = pets.map(pet => {
       if (pet.PetLikes.length) {
-        const petLike = pet.PetLikes.find(petLike => {
-          return petLike.userId === user.id
-          // console.log('petLikdId', petLike.id)
-          // console.log('petLikeUserId', petLike.userId)
-          // console.log('userId', user.id)
-          // console.log('------------------------------------------------')
-        })
-        if (petLike) {
-          return { exists: petLike.id }
-        } else {
-          return { exists: false }
+        for (let i = 0; i < pet.PetLikes.length; i++) {
+          if (pet.PetLikes[i].userId === user.id) {
+            return { exists: pet.PetLikes[i].id }
+          } else return { exists: false }
         }
+      } else {
+        return { exists: false }
       }
     })
   } else {
     petArr = [{ exists: false }]
   }
-  console.log('=============petArr', petArr)
+
 
   res.render('index', {
     title: 'PETsonalities',
